a67784fcf8e478414a78bbf4559368d01f46b7ba
Chloe <chloehiweb@gmail.com>
2024-10-18 17:13:52 +0200

    feat: creation de commande

---
+++ src/main/java/hiweb/conference/codepoem/api/Order.java
@@ -0,0 +1,23 @@
+package hiweb.conference.codepoem.api;
+
+public class Order {
+
+    public final String mail;
+    public final int quantity;
+    public final String address;
+
+    public Order(String mail, int quantity, String address) {
+        this.mail = mail;
+        this.quantity = quantity;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "email='" + mail + '\'' +
+                ", quantity=" + quantity +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/hiweb/conference/codepoem/api/OrderController.java
@@ -0,0 +1,25 @@
+package hiweb.conference.codepoem.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @GetMapping("/order")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String hello(
+            @RequestParam(value = "mail") String mail,
+            @RequestParam(value = "quantity") int quantity,
+            @RequestParam(value = "address") String address
+    ) {
+        Order order = new Order(mail, quantity, address);
+        return String.format("Commande de %s pairs de crocs de la part de %s. " +
+                "Adresse de livraison : %s.",
+                order.quantity, order.mail, order.address);
+    }
+
+}
