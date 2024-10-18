1229cb2429c80036342e39e302159cbd82a8ac84
Chloe <chloehiweb@gmail.com>
2024-10-18 16:19:17 +0200

    feat: project init

---
+++ src/test/java/hiweb/conference/codepoem/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hiweb.conference.codepoem.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
